acbe9de558e94ef526bbd9920574e795f2ae920d
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-04-27 16:50:27 +0200

    first commit

---
+++ src/fr/waytolearnx/Main.java
@@ -0,0 +1,12 @@
+package fr.waytolearnx;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    //1 - exercice 1 écrire : hello world !
+
+        System.out.println("hello world !");
+
+    }
+}
